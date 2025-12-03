72af6c56292dd8f812db2854bd1e687d7935e119
EXA_GAMER_007 <salimovsarvar518@gmail.com>
2025-12-03 17:49:32 +0500

    suuuuuuuu

---
+++ Salom.py
@@ -0,0 +1,6 @@
+def salomlashish(ism,fam,ota):
+    return f"Assalomu aleykum {ism} {fam} {ota} jamoamizga hush kelibsiz!"
+ism = input("Ismingizni kiriting: ")
+fam = input("Familyangizni kiriting: ")
+ota = input("Ochestvangizni kiriting: ")
+print(salomlashish(ism,fam,ota))
